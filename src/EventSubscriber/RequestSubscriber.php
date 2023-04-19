2187909223e632298cb350dd0544f5dc9c0940b3
Benjamin Komel <benjamin.komel@gmail.com>
2023-04-19 11:51:34 +0000

    Inject logger, config and keyvalue factories to RequestSubscriber

--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\scheduler_cron\EventSubscriber;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -12,6 +15,43 @@ use Symfony\Component\HttpKernel\KernelEvents;
 class RequestSubscriber implements EventSubscriberInterface {
 
   /**
+   * The config factory object.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The key value store to use.
+   *
+   * @var \Drupal\Core\KeyValueStore\KeyValueFactoryInterface
+   */
+  protected $keyValueFactory;
+
+  /**
+   * The logger channel factory.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $logger;
+
+  /**
+   * Creates a DiffFormatter to render diffs in a table.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \Drupal\Core\KeyValueStore\KeyValueFactoryInterface $key_value_factory
+   *   The key value store to use.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger
+   *   The logger channel factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, KeyValueFactoryInterface $key_value_factory, LoggerChannelFactoryInterface $logger) {
+    $this->configFactory = $config_factory;
+    $this->keyValueFactory = $key_value_factory;
+    $this->logger = $logger;
+  }
+
+  /**
    * Register running the Scheduler cronjob on shutdown if interval has passed.
    *
    * @param Symfony\Component\HttpKernel\Event\RequestEvent $event
@@ -19,7 +59,7 @@ class RequestSubscriber implements EventSubscriberInterface {
    */
   public function runSchedulerCron(RequestEvent $event) {
     // Get configuration.
-    $config = \Drupal::config('scheduler_cron.settings');
+    $config = $this->configFactory->get('scheduler_cron.settings');
     $interval = $config->get('interval');
     $log = $config->get('log');
     if (!\is_numeric($interval)) {
@@ -27,7 +67,7 @@ class RequestSubscriber implements EventSubscriberInterface {
     }
 
     // Get storage for last execution time.
-    $store = \Drupal::keyValue('scheduler_cron');
+    $store = $this->keyValueFactory->get('scheduler_cron');
     if (!$store->has('last')) {
       $store->set('last', 0);
     }
@@ -36,7 +76,7 @@ class RequestSubscriber implements EventSubscriberInterface {
     // shutdown function.
     if ($store->get('last') < time() - $interval * 60) {
       if ($log) {
-        \Drupal::logger('scheduler_cron')->notice('Executing scheduler cron on shutdown');
+        $this->logger->get('scheduler_cron')->notice('Executing scheduler cron on shutdown');
       }
       // Update execution time.
       $store->set('last', time());
